d43794e4617b479154de72aaaa5c09f160ef9da0
Srivarshini21 <srivarshinimavuri19@gmail.com>
2024-12-11 11:29:13 +0530

    first commit

---
+++ week6/hello.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class helloworld
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=100;i++)
+            {
+                int flag=0;
+                if(i==1)
+                {
+                    flag=1;
+                }
+                else
+                {
+                for(int j=2;j<=Math.sqrt(i);j++)
+                    {
+                        if(i%j==0)
+                        {
+                            flag=1;
+                            break;
+                        }
+                    }
+                }
+                if(flag==0)
+                    System.out.print(i+" ");
+            }
+    }
+}
